cee6460d0776b04bb024e958f2c82cc084a7bc1a
David Kim <davidcha@email.unc.edu>
2021-05-19 10:13:54 -0400

    Exercise 00

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,6 @@
-"""This is my first program for COMP 110"""
+"""This is my first program for COMP 110."""
 
 _author_ = "730407570"
-"""prints 'Hello world.'"""
 
+"""prints 'Hello world.'"""
 print("Hello world.")  
