205ddbfb1321cc28bbb7072d26525620aece3d86
David Kim <davidcha@email.unc.edu>
2021-05-23 16:06:27 -0400

    vaccine calculator

--- exercises/ex02/vaccine_calc.py
+++ exercises/ex02/vaccine_calc.py
@@ -1,7 +1,4 @@
 """A vaccination calculator."""
-
-__author__: str = "730407570"
-
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
 #
@@ -16,17 +13,22 @@ from datetime import datetime
 # Official Documentation: https://docs.python.org/3/library/datetime.html#datetime.timedelta
 from datetime import timedelta
 
-
+__author__: str = "730407570"
 # Begin your solution here...
 population: str = input("Population: ")
 doses_administered: str = input("Doses administered: ")
 doses_per_day: str = input("Doses per day: ")
-target_percent_vaccinated: str = input("Target percent vaccinated: ")
+t_vax: str = input("Target percent vaccinated: ")
 
-unvaccinated_pop: float = int(population) - int(target_percent_vaccinated) * .01 * int(population)
-days_to_target: float = unvaccinated_pop / int(doses_per_day)
+target_pop: float = int(population) * int(t_vax) * .01
+vaccinated_pop: float = int(doses_administered) / 2
+unvaccinated_pop: float = int(population) - vaccinated_pop
+goal_pop: float = target_pop - vaccinated_pop
+g_days: int = int(round(goal_pop * 2 / int(doses_per_day)))
 
 today: datetime = datetime.today()
-days_to_target_date: timedelta = timedelta(days_to_target)
-target_date: datetime = today + days_to_target_date
-print("We will reach " + str(target_percent_vaccinated) + "% vaccination in " + str(int(days_to_target)) + " days, which falls on " + target_date.strftime( "%B %d, %Y."))
+days_to_goal_date: timedelta = timedelta(g_days)
+t_date: datetime = today + days_to_goal_date
+
+print("We will reach " + (t_vax) + "% vaccination in "
+      + str(g_days) + " days, which falls on " + t_date.strftime("%B %d, %Y."))
