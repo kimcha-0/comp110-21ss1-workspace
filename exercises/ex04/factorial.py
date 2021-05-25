c83233c3e83ad141d490f1af37cac01f70c5f619
David Kim <davidcha@email.unc.edu>
2021-05-25 18:00:55 -0400

    exercise 4 complete

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -5,3 +5,11 @@ __author__ = "730407570"
 
 # Begin your solution here...
 number: int = int(input("Choose a number: "))
+
+integer: int = 1
+
+while number > 0:
+    integer *= number
+    number -= 1
+
+print(integer)
--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -1,6 +1,18 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730407570"
 
 
 # Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")
+repeat_num: int = int(input("How many times do you want to repeat it? "))
+
+total_beat: str = beat
+
+if repeat_num <= 0:
+    print("No beat...")
+else:
+    while repeat_num > 1:
+        total_beat = total_beat + " " + beat
+        repeat_num -= 1
+    print(total_beat)
