31422a6c475657e7b1487d8dd59cd37b445cd92a
David Kim <davidcha@email.unc.edu>
2021-05-29 19:00:45 -0400

    project 00

--- projects/pj00.py
+++ projects/pj00.py
@@ -1,15 +1,20 @@
 """A number guessing game."""
 
-def main() -> None:
-    """Entrypoint of the program.""" 
-    greet()   
-    points: int = 0
-    return None
-
-
 def greet() -> None:
     print("Welcome to guess the number! A game where you, the user, guesses a number!")
     player: str = input("What is your name? ")
+    return None
+
+
+def main() -> None:
+    """Entrypoint of the program.""" 
+    greet()
+    points: int = 0
+    print("Now, you have three options: Exit,"
+    choice: str =  input("What do you choose? ")
+    if choice == "Exit" or "exit":
+        print(f"Thanks for checking us out, we hope you come back again. Points: {points}.")
+    return None
 
 
 if __name__ == "__main__":
