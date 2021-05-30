6692ffd17c4bf483d59cd7ba33a81557f74ac511
David Kim <davidcha@email.unc.edu>
2021-05-30 01:12:23 -0400

    looping game

--- lessons/magic_8_ball.py
+++ lessons/magic_8_ball.py
@@ -5,10 +5,17 @@ from random import randint
 question: str = input("Ask a yes/no question! ")
 response: int = randint(0,2)
 
-if response == 0:
-    print("Very doubtful.")
-else:
-    if response == 1:
-        print("Ask again later.")
+is_playing: bool = True
+while is_playing:
+    if response == 0:
+        print("Very doubtful.")
+    elif response == 1:
+            print("Ask again later.")
     else:
         print("It is certain.")
+    
+    play_again: str = input("Play again? yes/no ")
+    if play_again == "no":
+        is_playing = False
+
+print("Have a great day!")
