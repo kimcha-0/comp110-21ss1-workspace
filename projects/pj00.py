8960698671ccea766897e45517994d3ee4b7204d
David Kim <davidcha@email.unc.edu>
2021-05-29 22:22:57 -0400

    project 00

--- projects/pj00.py
+++ projects/pj00.py
@@ -1,23 +1,24 @@
 """A number guessing game."""
 from random import randint
 
+points: int = 0
+
+
 def main() -> None:
     """Entrypoint of the program and starts the game.""" 
-    greet()
     points: int = 0
-    print("Now, you have three options: Exit, Play, Play a different game")
-    choice: str = input("What do you choose? ")
-    if choice == "Exit" or "exit":
-        print(f"Thanks for checking us out, we hope you come back again. Points: {points}")
-    elif choice == "Play" or "play":
+    greet()
+    print("Now, you have three options: Exit (type 0), Play version 1 (type 1), Play version 2 (type 2)")
+    option: int = int(input("Type in your choice (0, 1, 2): "))
+    if option == 0:
+        print(f"Thanks for checking us out. We hope to see you again. Points: {points}")
+    elif option == 1:
         print(f"Let's begin. Points: {points}")
         game()
     else:
-        print(f"Let's play! Points: {points}")
+        print(f"Let's begin. Points: {points}")
         other_game()
-
     return None
-
 
 def greet() -> None:
     print("Welcome to guess the number! A game where you, the user, guesses a number!")
@@ -25,14 +26,13 @@ def greet() -> None:
     ver_1: str = "In version one, you guess a number between 1-10."
     ver_2: str = "In version two, you guess a number between 1-100!"
     print(f"{player}, there are two versions for you to play. {ver_1} {ver_2}")
-    
     return None
 
 
 def game() -> int:
     """Generates a random integer 1-10. If player is correct, add 10 points."""
     answer: int = randint(1,10)
-    return 
+    attempt: int = int(input("Take a guess: "))
 
 
 def other_game(x: int) -> int:
