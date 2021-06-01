e30e3dafe1e4f16ebb79c620750024856845fc59
David Kim <davidcha@email.unc.edu>
2021-06-01 09:44:43 -0400

    Testing Prime numbers

--- exercises/ex07/primes.py
+++ exercises/ex07/primes.py
@@ -1,15 +1,17 @@
 """An exercise with functions and lists."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730407570"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     # TODO 3: Test your functions here
-
+    print(is_prime())
+    return None
 
 # TODO 1: Define the is_prime function, and its logic, here.
-
+def is_prime(a: int) -> bool:
+    return True
 # TODO 2: Define the list_primes function, and its logic, here.
 
 
