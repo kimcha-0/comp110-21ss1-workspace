1f7639e3da887aa65030c913e4ba9a25aae97388
David Kim <davidcha@email.unc.edu>
2021-06-01 11:08:07 -0400

    exercise 07

--- exercises/ex07/primes.py
+++ exercises/ex07/primes.py
@@ -6,15 +6,20 @@ __author__ = "730407570"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     # TODO 3: Test your functions here
-    print(is_prime())
     return None
 
 # TODO 1: Define the is_prime function, and its logic, here.
-def is_prime(a: int) -> bool:
-    
-    return True
+def is_prime(n: int) -> bool:
+    i: int = 1
+    while i < n:
+        """Testing for primes logic."""
+        return True
+    return False
 # TODO 2: Define the list_primes function, and its logic, here.
-
+def list_primes(start: int, stop: int) -> list[int]:
+    i: int = start
+    result: list[int] = []
+    return result
 
 if __name__ == "__main__":
     main()
