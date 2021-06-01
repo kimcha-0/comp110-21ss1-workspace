2f1f1840409b51af1d16e16f73e0f18e0336d9ff
David Kim <davidcha@email.unc.edu>
2021-06-01 03:10:38 -0400

    Project 00

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -5,6 +5,7 @@ from random import randint
 
 points: int = 0
 player: str = ""
+happy_face = "\U0001F600"
 
 def main() -> None:
     """Entrypoint of the program and starts the game.""" 
@@ -19,7 +20,7 @@ def main() -> None:
     elif option == 1:
         print(f"Let's begin. Points: {points}")
         while is_playing: 
-            game()
+            game(points)
             play_again: str = input("Play again? yes/no ")
             points += 10
             if play_again == "no":
@@ -29,7 +30,7 @@ def main() -> None:
     else:
         print(f"Let's begin. Points: {points}")
         while is_playing:
-            other_game()
+            other_game(points)
             play_again: str = input("Play again? yes/no ")
             points += 10
             if play_again == "no":
@@ -38,7 +39,7 @@ def main() -> None:
     return None
 
 def greet() -> None:
-    print("Welcome to guess the number! A game where you, the user, guesses a number!")
+    print(f"Welcome to guess the number! A game where you, the user, guesses a number {happy_face}!")
     player: str = input("What is your name? ")
     ver_1: str = "In version one, you guess a number between 1-10."
     ver_2: str = "In version two, you guess a number between 1-20!"
@@ -47,11 +48,11 @@ def greet() -> None:
     return None
 
 
-def game() -> None:
+def game(a: int) -> int:
     """Generates a random integer 1-10. If player is correct, add 10 points. 
     Returns the final point value to the function."""
     attempts: int = 1
-    i = 10
+    i = 100
     answer: int = randint(1,10)
     
     while attempts < i:
@@ -62,13 +63,13 @@ def game() -> None:
         else: 
             print(f"Correct you guessed the correct number in {attempts} attempts and you receive 10 points.")
             break
-            
-    return None
+    a += 10
+    return a
 
 
-def other_game() -> None:
+def other_game(a: int) -> None:
     attempts: int = 1
-    i = 20
+    i = 100
     answer: int = randint(1,20)
 
     while attempts < i:
@@ -79,7 +80,8 @@ def other_game() -> None:
         else:
             print(f"Correct you guessed the correct number in {attempts} attempts and you receive 10 points.")
             break
-    return None
+    a += 10
+    return a
 
 if __name__ == "__main__":
     main()
