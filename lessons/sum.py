fe3023ace6419b6b75cc6ecaadc16b16d0de4006
David Kim <davidcha@email.unc.edu>
2021-06-01 10:34:34 -0400

    summation algorithm

---
+++ lessons/sum.py
@@ -0,0 +1,9 @@
+"""Sums the elements of a list."""
+
+def sum(xs: list[int]) -> int:
+    """Summation of input list is returned."""
+    total: int = 0
+    i: int = 0
+    while i < len(xs):
+        total += i
+    return total
