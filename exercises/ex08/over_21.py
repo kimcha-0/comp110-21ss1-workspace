0c35d34a962c6310c64e801e4fabfc0a6910d1ef
David Kim <davidcha@email.unc.edu>
2021-06-02 16:52:18 -0400

    over 21

--- exercises/ex08/over_21.py
+++ exercises/ex08/over_21.py
@@ -1,12 +1,12 @@
 """A program to determine names over 21."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730407570"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     # TODO 2: Test your function here
-
+    return None
 
 # TODO 1: Define the over_21 function, and its logic, here.
 
