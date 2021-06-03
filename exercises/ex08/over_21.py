0790393c0fb6552f48f4391df56c0f82d5b97070
David Kim <davidcha@email.unc.edu>
2021-06-03 10:02:04 -0400

    ex08

--- exercises/ex08/over_21.py
+++ exercises/ex08/over_21.py
@@ -9,7 +9,14 @@ def main() -> None:
     return None
 
 # TODO 1: Define the over_21 function, and its logic, here.
-
+def over_21(names: dict[str, int]) -> list[str]:
+    """Takes a dictionary with names with their corresponding ages and returns a list with the people
+    with ages over 21."""
+    results: list[str] = []
+    for age in names:
+        if names[age] > 21:
+            results.append(age)
+    return results
 
 if __name__ == "__main__":
     main()
