99eb28d979637c588faa2013fb4480e9d25268d7
David Kim <davidcha@email.unc.edu>
2021-06-07 11:55:16 -0400

    data analysis functions

--- exercises/ex09/data_utils.py
+++ exercises/ex09/data_utils.py
@@ -4,6 +4,56 @@ __author__ = "YOUR 9-DIGIT PID"
 
 
 #TODO 0: Paste the functions we've been working on in class, here.
+def read_csv_rows(path: str) -> list[dict[str, str]]:
+    """Read a CSV file and return a list of its rows."""
+    file_handle = open(path, encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    table: list[dict[str, str]] = []
+    for row in csv_reader:
+        table.append(row)
+    return table
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Accesses an arbitraty column and appends the values to a list."""
+    values: list[str] = []
+    for row in table:
+        values.append(row[column])
+    return values
+
+# Let's convert our data to column-oriented
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Convert a table of rows to a table of columns."""
+    result: dict[str, list[str]] = {}
+    keys = table[0].keys()
+    for key in keys:
+        print(f"The key is {key}.")
+        result[key] = column_values(table, key)
+    return result
+
+
+def head(table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
+    """Get first n rows."""
+    result: dict[str, list[str]] = {}
+    for key in table:
+        result[key] = table[key][:rows]
+    return result
+
+def select(table: dict[str, list[str]], cols: list[str]) -> dict[str, list[str]]:
+    """Select a subset of columns."""
+    result: dict[str, list[str]] = {}
+    for col_name in cols:
+        result[col_name] = table[col_name]
+    return result
 
 
 # TODO 1: Define the count function, and its logic, here.
+def count(x: list[str]) -> dict[str, int]:
+    result: dict[str, int] = {}
+    for i in x:
+        if i in result:
+            result[i] += 1
+        else:
+            result[i] = 1
+    return result
