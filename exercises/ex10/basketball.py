230f9e47d0d04350a9ec3e7f4287b9d4ace30a0c
David Kim <davidcha@email.unc.edu>
2021-06-08 21:34:47 -0400

    basic objecgs

--- exercises/ex10/basketball.py
+++ exercises/ex10/basketball.py
@@ -4,6 +4,21 @@ __author__ = "YOUR 9-DIGIT PID"
 
 
 # TODO 1: Define the BBallGame class, and its logic, here.
+class BBallGame:
+    """Represents a sim of a basketball game."""
+    biscuits: bool 
+    points: int
+    winning_team: str
+    losing_team: str
+    def __init__(self, biscuits: bool, points: int, winning_team: str, losing_team: str):
+        self.biscuits = False
+    def check_points(self) -> None:
+        return None
+    def winner(self) -> str:
+        return 
+    def reset_points(self) -> int:
+        return 
+
 
 
 
