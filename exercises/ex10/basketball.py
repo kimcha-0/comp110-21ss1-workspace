66a234070f5f5a84e78d8ec1cbf6e1589f49bbc3
David Kim <davidcha@email.unc.edu>
2021-06-08 22:43:54 -0400

    basic objects

--- exercises/ex10/basketball.py
+++ exercises/ex10/basketball.py
@@ -10,8 +10,35 @@ class BBallGame:
     points: int
     winning_team: str
     losing_team: str
-    def __init__(self, biscuits: bool, points: int, winning_team: str, losing_team: str):
+    def __init__(self, points: int, winning_team: str, losing_team: str):
+        """Construct values for the attributes."""
         self.biscuits = False
+        self.points = points 
+        self.winning_team = winning_team
+        self.losing_team = losing_team
+    
+    def check_points(self) -> None:
+        """checks if points scored are greater than 100."""
+        if self.points >= 100: 
+            self.biscuits = True
+    def winner(self) -> str:
+        """Returns different strings based on who the winning team is."""
+        beat_duke: str = "GTHD!!"
+        win: str = "woohoo"
+        loss: str = "daggum"
+        if self.winning_team == "UNC" and self.losing_team  == "Dook":
+            return beat_duke
+        elif self.winning_team == "UNC" and self.losing_team != "Dook":
+            return win
+        else:
+            return loss
+
+    def reset_points(self) -> int:
+        """Resets point total to 0."""
+        result: self.points
+        self.points = 0
+        return result
+
 
 
 
