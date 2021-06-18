6277190b15ee3fc240d7f6833da20b93938b9b40
David Kim <davidcha@email.unc.edu>
2021-06-17 20:56:47 -0400

    linked list recursive functions

--- exercises/ex16/linked_list_part1.py
+++ exercises/ex16/linked_list_part1.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 __author__ = "730407570"
 
+
 class Node:
     """A single link in a linked list."""
     data: int
@@ -21,3 +22,39 @@ class Node:
             return f"{self.data} -> None"
         else:
             return f"{self.data} -> {self.next}"
+
+
+def last(head: Optional[Node]) -> Optional[int]:
+    """Returns the last value of a linked list, or None if the list is empty."""
+    if head is None:
+        return None
+    elif head.next is None:
+        return head.data
+    else:
+        return last(head.next)
+
+
+def value_at(head: Optional[Node], index: int) -> Optional[int]:
+    """Returns the value at the given index of a linked list, or None if the list is empty."""
+    if head is None:
+        return None
+    else:
+        if index == 0:
+            return head.data
+        else:
+            return value_at(head.next, index - 1)
+
+
+def max(head: Optional[Node]) -> Optional[int]:
+    """Return the smallest value in a linked list."""
+    if head is None:
+        return None
+    else:
+        max_rest = max(head.next)
+        if max_rest is None:
+            return head.data
+        else:
+            if head.data > max_rest:
+                return head.data
+            else:
+                return max_rest
