2813a3aa788e789ffef09f3c2b22d9242c216670
David Kim <davidcha@email.unc.edu>
2021-06-21 14:24:01 -0400

    linked list methods

--- exercises/ex17/linked_list_part2.py
+++ exercises/ex17/linked_list_part2.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 from typing import Optional
 
-__author__ = "YOUR PID HERE"
+__author__ = "730407570"
 
 class Node:
     """A single link in a linked list."""
@@ -21,3 +21,46 @@ class Node:
             return f"{self.data} -> None"
         else:
             return f"{self.data} -> {self.next}"
+
+def linkify(items: list[int]) -> Optional[Node]:
+    """Converts a list into a linked list."""
+    # edge case
+    if len(items) == 0:
+        return None
+    # base case
+    elif len(items) == 1:
+        return Node(items[0], None)
+    return Node(items[0], linkify(items[1:]))
+
+
+def scale(head: Optional[Node], factor: int) -> Optional[Node]:
+    """Returns a new linked list with elements multiplied by 'factor'."""
+    if head is None:
+        return None
+    else:
+        first = head.data * factor
+        rest = scale(head.next, factor)
+        return Node(first, rest)
+
+
+def concat(lhs: Optional[Node], rhs: Optional[Node]) -> Optional[Node]:
+    """Returns a new linked list where the first list is followed by the second."""
+    # base case
+    if lhs is None:
+        return rhs
+    if rhs is None:
+        return lhs
+    elif lhs is None and rhs is None:
+        return None
+    # recursive case
+    else:
+        first = lhs.data
+        rest = concat(lhs.next, rhs)
+        return Node(first, rest)
+
+x: Optional[Node] = Node(1, Node(2, Node(3, Node(4, Node(5, None)))))
+y: list[int] = [1, 2, 3, 4, 5]
+z: Optional[Node] = Node(6, Node(7, Node(8, Node(9, None))))
+print(linkify(y))
+print(scale(x, 2))
+print(concat(x, z))
